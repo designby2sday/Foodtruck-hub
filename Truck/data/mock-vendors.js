cd0e747e679be4547aea02c0915c36fa172f02c4
designby2sday <designsby2sday@gmail.com>
2025-08-02 20:23:17 -0400

    created the dev-sitemap

---
+++ Truck/data/mock-vendors.js
@@ -0,0 +1,22 @@
+const mockVendors = [
+  {
+    id: 1,
+    name: "Downtown Events Co.",
+    city: "Farmville",
+    contact: "info@downtownevents.com"
+  },
+  {
+    id: 2,
+    name: "Richmond Events LLC",
+    city: "Richmond",
+    contact: "events@richmondco.com"
+  },
+  {
+    id: 3,
+    name: "Harvest Group",
+    city: "Lynchburg",
+    contact: "harvest@hg.com"
+  }
+];
+
+export default mockVendors;
